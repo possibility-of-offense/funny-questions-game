8ec7f3c0ce45de9b776c86655e0432a42d936e35
Ventsislav Iliev <ultimatemeaning90@gmail.com>
2022-05-19 16:58:44 +0300

    trying

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
   // </React.StrictMode>
